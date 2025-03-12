35416b28e2e8391b7485493a9a9fd865a5acc96e
os-dathx032025 <os-dathx032025@sacombank.corp.vn>
2025-03-12 17:03:40 +0700

    first commit

---
+++ src/main/java/com/redis/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.redis.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.redis.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
